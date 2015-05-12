a944a1c2a3340d77b140a03b6d3c12b9f6f710e5
clid <clid@kth.se>
2015-05-12 12:14:46 +0200

    Fix in termination print

--- semant/PrettyPrinter.java
+++ semant/PrettyPrinter.java
@@ -218,7 +218,11 @@ public class PrettyPrinter implements WhileVisitor {
                 System.out.print(" (possibly exceptional termination");
             else
                 System.out.print(" (normal termination)");
-        } else
-            System.out.print(" (exceptional termination)");
+        } else {
+            if(vm.possiblyNormalTermination())
+                System.out.print(" (exceptional termination)");
+            else
+                System.out.print(" (no termination)");
+        }
     }
 }
